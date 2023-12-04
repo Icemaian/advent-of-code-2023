dde054580c84af06ea156518a5f42359d9dd8565
isaac craig <isaac_michael_craig@hotmail.com>
2023-12-03 23:29:29 -0500

    wip...

--- day-3/Gear-Ratios/GearRatios.py
+++ day-3/Gear-Ratios/GearRatios.py
@@ -1,6 +1,4 @@
 
-
-
 def listContainsSymbol(input: list) -> bool:
     invalidChars = {'.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
     for char in input:
@@ -9,3 +7,50 @@ def listContainsSymbol(input: list) -> bool:
         else:
             return True
     return False
+
+def processLine(currLine: list, nextLine: list = [], prevLine: list = []) -> int:
+    result = 0
+    canidate = ""
+    for index, char in enumerate(currLine):
+        if char.isnumeric(): #If current char is digit add and continue
+            canidate = canidate + char 
+        elif canidate and not char.isnumeric(): #If we have a canidate but our current char check if validity of canidate
+            startPoint = index-len(canidate)-1 if index-len(canidate)-1 > 0 else 0
+            stopPoint = index+1
+            if (
+                    listContainsSymbol(currLine[startPoint:stopPoint]) or   #Check the current char & the char before canidate
+                    listContainsSymbol(nextLine[startPoint:stopPoint]) or #Check row below extended in either directio +1
+                    listContainsSymbol(prevLine[startPoint:stopPoint])    #Check row below extended in either directio +1 
+                    ):
+                result += int(canidate)
+            canidate = ""
+    return result
+
+def openData(fPath: str) -> int:
+    total = 0
+    currLine = []
+    nextLine = []
+    prevLine = []
+    lineIndex = 0
+    with open(fPath, 'r') as f:
+        for line in f:
+            line = list(line.replace("\n", ""))
+            lineIndex += 1
+            if len(currLine) == 0:
+                currLine = list(line)
+            else:
+                nextLine = list(line)
+                total += processLine(currLine, nextLine, prevLine)
+                prevLine = currLine
+                currLine = nextLine
+    total += processLine(currLine, [], prevLine) #Process last line
+    print(f'Total: {total}')
+    return total
+
+
+
+                
+                
+
+
+
--- day-3/Gear-Ratios/GearRatios.unittest.py
+++ day-3/Gear-Ratios/GearRatios.unittest.py
@@ -1,11 +1,25 @@
-import unittest                                                                                     from GearRatios import listContainsSymbol
+import unittest
+from GearRatios import listContainsSymbol, processLine, openData
 
 class filenameUnitTest(unittest.TestCase):
     def test_invalidChars(self):
         test_false_input = ['.', '.', '.', '8']
         test_true_input = ['.', '.', '#', '.']
         self.assertEqual(False, listContainsSymbol(test_false_input))
-        self.assertEqual(True, listContainsSymbol(test_true_input))
+    def test_processline(self):
+        test_currLine = ['4','6','7','.','.','1','1','4','.','.']
+        test_nextLine = ['.','.','.','*','.','.','.','.','.','.']
+        test_prevLine = []
+        test_answer = 467
+        self.assertEqual(test_answer, processLine(test_currLine, test_nextLine, test_prevLine))
+    
+    def test_openData(self):
+        test_input = "test.input"
+        test_answer = 4361
+        self.assertEqual(test_answer,openData(test_input))
 
+    def test_puzzel(self):
+        test_input = "GearRatios.input"
+        self.assertIsNotNone(openData(test_input))
 if __name__ == '__main__':
     unittest.main()
