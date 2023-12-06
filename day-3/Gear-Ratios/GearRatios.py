2c4b7d88019441a27d96313f2856fbf14744677e
isaac craig <isaac_michael_craig@hotmail.com>
2023-12-05 19:26:45 -0500

    ignoring input files...

--- day-3/Gear-Ratios/GearRatios.py
+++ day-3/Gear-Ratios/GearRatios.py
@@ -1,6 +1,5 @@
 
 def listContainsSymbol(input: list) -> bool:
-    print(f'checking if line has symbols... {input}')
     invalidChars = {'.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
     for char in input:
         if char in invalidChars:
@@ -14,11 +13,9 @@ def processLine(currLine: list, nextLine: list = [], prevLine: list = []) -> int
     canidate = ""
     for index, char in enumerate(currLine):
         readyToValidate = (not char.isnumeric() or index==len(currLine)-1)
-        print(f'Handling char {char}, at index {index}, {len(currLine)-1} if statement {readyToValidate}')
         if char.isnumeric(): #If current char is digit add and continue
             canidate = canidate + char 
         if canidate and readyToValidate: #If we have a canidate but our current char check if validity of canidate
-            print(f'Checking validity for canidate {canidate}')
             startPoint = index-len(canidate)-1 if index-len(canidate)-1 > 0 else 0
             stopPoint = index+1
             if (
@@ -27,13 +24,13 @@ def processLine(currLine: list, nextLine: list = [], prevLine: list = []) -> int
                     
                     listContainsSymbol(nextLine[startPoint:stopPoint])    #Check row below extended in either directio +1 
                     ):
-                print(f'Canidate: {canidate} is valid adding to total {result} = {result + int(canidate)}')
                 result += int(canidate)
             canidate = ""
     return result
 
 def openData(fPath: str) -> int:
     total = 0
+    totalGear = 0
     currLine = []
     nextLine = []
     prevLine = []
@@ -47,20 +44,47 @@ def openData(fPath: str) -> int:
             else:
                 nextLine = list(line)
                 total += processLine(currLine, nextLine, prevLine)
+                #totalGear += findGear(currLine, nextLine, prevLine)
                 prevLine = currLine
                 currLine = nextLine
-                print('---------------------------------------------------')
-                print(f'Finished currLine {lineIndex} for a total value: {total}')
-                print('---------------------------------------------------')
-
+    #totalGear += findGear(currLine, nextLine, prevLine)
     total += processLine(currLine, [], prevLine) #Process last line
-    print(f'Total: {total}')
-    return total
+    print(f'Total: {total}, total Gear {totalGear}')
+    return total 
 
 
+def findGear(currLine: list, nextLine: list = [], prevLine: list = []) -> int:
+    result = 0
+    for index, char in enumerate(currLine):
+        if char == "*":
+            print(f"Potential Gear found at {index-1}")
+            startPoint = index-4 if index-4 >= 0 else 0
+            endPoint = index+3
+            canidates = []
+            for line in [currLine, nextLine, prevLine]:
+                canidate = gearValue(line, index-1)
+                print(canidate)
+                #`canidates.append(*canidate) if len(canidate) > 0 else None
+            if len(canidates) == 2:
+                result += (canidates[0] * canidates[1])
+    return result
 
-                
-                
+def gearValue(line: list, index: int) -> int:
+    result = []
+    canidate = ""
+    for idx, char in enumerate(line):
+        readyToValidate = (not char.isnumeric() or idx == len(line)) # check if we've reached the end of the digit or line
+        if char.isnumeric():
+            canidate += char
+        if canidate and readyToValidate:
+            if index >= idx-len(canidate) and index <= idx+1:
+                result.append(int(canidate))
+            else:
+                canidate = ""
+    return result
+        
+
+
 
 
 
--- day-3/Gear-Ratios/GearRatios.unittest.py
+++ day-3/Gear-Ratios/GearRatios.unittest.py
@@ -1,5 +1,5 @@
 import unittest
-from GearRatios import listContainsSymbol, processLine, openData
+from GearRatios import listContainsSymbol, processLine, openData, gearValue, findGear
 
 class filenameUnitTest(unittest.TestCase):
     def test_invalidChars(self):
@@ -22,5 +22,18 @@ class filenameUnitTest(unittest.TestCase):
         test_input = "GearRatios.input"
         test_answer = 530849
         self.assertEqual(test_answer,openData(test_input))
+
+    def test_gearValue(self):
+        test_input = (['7','.','.','1','1','4','.'], 3)
+        test_answer = [114]
+        self.assertEqual(test_answer, gearValue(test_input[0], test_input[1]))
+    
+    def test_findGear(self):
+        test_currLine = ['.','.','.','*','.','.','.','.','.','.']
+        test_nextLine = ['4','6','7','.','1','1','4','.','.','.']
+        test_prevLine = []
+        test_answer = 53238
+        self.assertEqual(test_answer, findGear(test_currLine, test_nextLine, test_prevLine))
+
 if __name__ == '__main__':
     unittest.main()
